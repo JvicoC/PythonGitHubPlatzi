f1188b0235615ed639237c15c1ebb644fdc39acf
Vicolas666 <jvicoc@gmail.com>
2024-08-24 17:30:10 -0400

    ultimos cambios con la web-server con las librerias fast api

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+#$ uvicorn main:app --reload
+#CODIGO PARA HACER LA PRUEBA ESTE MAIN EN SHELL
 
-def run():
-    store.get_categories()
+app= FastAPI()
 
-if __name__=='__main__':
-    run()
+@app.get('/') #decorador ruta desde la web
+def get_list():
+    return [1,2,3]
+
+@app.get('/usuarios',response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>"HOLA MUNDO"</h1>
+        <h2>soy vico vICOLAS</h2>        
+    """
+#def run():
+ #   store.get_categories()
+
+#if __name__=='__main__':
+ #   run()
